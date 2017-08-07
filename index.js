fc829f2ec93cdc6c9cef5820ab867f28da441c0d
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-08-07 00:04:19 -0700

    Added prefix to commands. Added ping command. Removed basic hello. Fixed #2.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+// Tyler Radabaugh
+
 // Import the discord.js module
 const Discord = require('discord.js');
 
 // Create an instance of a Discord client
 const bot = new Discord.Client();
+const TOKEN = 'MzQzOTM1NjM2MTAyODQwMzIw.DGllPQ.-y7TRmdyoxbHeFO21NbfiN6pLxk';
+const PREFIX = "!";
 
-// The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'MzQzOTM1NjM2MTAyODQwMzIw.DGllPQ.-y7TRmdyoxbHeFO21NbfiN6pLxk';
-
-// The ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted
+// The ready event 
 bot.on("ready", function() {
     console.log("Ready");
 });
@@ -16,10 +16,16 @@ bot.on("ready", function() {
 bot.on("message", function(message) {
     if (message.author.equals(bot.user)) return;
 
-    if (message.content == "!hello" || message.content == "!Hello") {
-        message.channel.sendMessage("Hi, how can I help?");
+    if (!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0]) {
+    	case "ping":
+    		message.channel.sendMessage("Pong!");
+    		break;
     }
 });
 
 // Log our bot in
-bot.login(token);
+bot.login(TOKEN);
