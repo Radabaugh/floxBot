1871442819df62b0e0f015cb8f9edd27929add8c
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-08-07 15:46:41 -0700

    Added ability to play music Fixed #9

--- index.js
+++ index.js
@@ -2,11 +2,28 @@
 
 // Import the discord.js module
 const Discord = require('discord.js');
+const YTDL = require("ytdl-core");
+
+function play(connection, message) {
+	var server = servers[message.guild.id];
+
+	server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+
+	server.queue.shift();
+
+	server.dispatcher.on("end", function() {
+		if (server.queue[0]) play(connection, message);
+		else connection.disconnect();
+	});
+}
 
 // Create an instance of a Discord client
 const bot = new Discord.Client();
 const TOKEN = 'MzQzOTM1NjM2MTAyODQwMzIw.DGllPQ.-y7TRmdyoxbHeFO21NbfiN6pLxk';
 const PREFIX = "!";
+
+// Servers for multiple discord music queues
+var servers = {};
 
 // The ready event 
 bot.on("ready", function() {
@@ -42,6 +59,40 @@ bot.on("message", function(message) {
     			.addField("ping", "Play ping-pong with Flox Bot! Or just see if he's online.")
     			.setColor(0xff6500);
     		message.channel.sendEmbed(embed);
+    		break;
+    	case "play":
+    		if (!args[1]) {
+    			message.channel.sendMessage("Please provide a link.");
+    			return;
+    		}
+
+    		if (!message.member.voiceChannel) {
+    			message.channel.sendMessage("You must be in a voice channel.");
+    			return;
+    		}
+
+    		if (!servers[message.guild.id]) servers[message.guild.id] = {
+    			queue: []
+    		};
+
+    		var server = servers[message.guild.id];
+
+    		server.queue.push(args[1]);
+
+    		if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+    			play(connection, message);
+    		});
+    		break;
+    	case "skip":
+    		var server = servers[message.guild.id];
+
+    		if (server.dispatcher) server.dispatcher.end();
+    		break;
+    	case "stop":
+    		var server = servers[message.guild.id];
+
+    		if (server.dispatcher) server.dispatcher.end();
+    		if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect;
     		break;
     	default:
     		message.channel.sendMessage("Invalid command.");
